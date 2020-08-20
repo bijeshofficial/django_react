29b04e7d198d0d5460e43a37407bc80d429aee01
bijeshofficial <bijeshr.kunwar@gmail.com>
2020-08-20 17:26:27 +0545

    finished error handling and alerts for post and delete request

--- frontend/src/components/layout/Alerts.js
+++ frontend/src/components/layout/Alerts.js
@@ -6,10 +6,11 @@ import PropTypes from "prop-types";
 export class Alerts extends Component {
   static propTypes = {
     error: PropTypes.object.isRequired,
+    message: PropTypes.object.isRequired,
   };
 
   componentDidUpdate(prevProps) {
-    const { error, alert } = this.props;
+    const { error, alert, message } = this.props;
     if (error !== prevProps.error) {
       if (error.msg.name) {
         alert.error(`Name: ${error.msg.name.join()}`);
@@ -21,6 +22,15 @@ export class Alerts extends Component {
         alert.error(`Message: ${error.msg.message.join()}`);
       }
     }
+
+    if (message !== prevProps.error) {
+      if (message.deleteLead) {
+        alert.success(message.deleteLead);
+      }
+      if (message.addLead) {
+        alert.success(message.addLead);
+      }
+    }
   }
 
   render() {
@@ -30,6 +40,7 @@ export class Alerts extends Component {
 
 const mapStateToProps = (state) => ({
   error: state.errors,
+  message: state.messages,
 });
 
 export default connect(mapStateToProps)(withAlert()(Alerts));
