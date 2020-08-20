c8e2c27a4fe23c5e06caad26fdfbd037df1dea26
bijeshofficial <bijeshr.kunwar@gmail.com>
2020-08-20 19:44:39 +0545

    added requirements.txt file

--- leads/api.py
+++ leads/api.py
@@ -1,9 +1,20 @@
 from .models import Lead
 from rest_framework import viewsets, permissions
 from .serializers import LeadSerializer
+from django.core.exceptions import PermissionDenied
 
-#Lead Viewset
+# Lead Viewset
+
+
 class LeadViewSet(viewsets.ModelViewSet):
-    queryset = Lead.objects.all()
+    # queryset = Lead.objects.all()
     serializer_class = LeadSerializer
-    permissions_classes = [permissions.AllowAny]
+    permissions_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        if not self.request.user.is_authenticated:
+            raise PermissionDenied()
+        return self.request.user.leads.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
